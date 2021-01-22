946d6dc26a0917a419e6b7f78824990158782680
stabishali8 <stabishali8@gmail.com>
2021-01-23 00:57:25 +0500

    Complete React Tutorial (& Redux) #27 - Programmatic Redirects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,11 @@ const Navbar = (props) =>{
             <div className="container">
                 <a className="brand-logo">Poke'Times</a>
                 <ul className="right">
+
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
+                
                 </ul>
             </div>
         </nav>   
